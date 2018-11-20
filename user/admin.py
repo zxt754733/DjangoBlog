18d324f28d70773947d6ed5258a08e2173168e2a
Unknown <502838517@qq.com>
2018-11-20 17:41:20 +0800

    自定义用户模型和后台站点的前端显示

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 
-# Register your models here.
+from .models import Profile
+
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+
+
+class UserAdmin(BaseUserAdmin):
+    inlines = (ProfileInline,)
+    list_display = ('username', 'nickname', 'email', 'is_active', 'is_superuser')
+
+    def nickname(self, obj):
+        return obj.profile.nickname
+    nickname.short_description = '昵称'  # 设置后台中文显示
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'nickname')
